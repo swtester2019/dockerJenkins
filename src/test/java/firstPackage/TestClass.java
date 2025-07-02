7b69c5ffb52189f4c18b3bf11d9ac761cb8e6433
Aleksei Lepekhin <aleksey.lepekhin@gmail.com>
2025-07-02 15:56:10 +0400

    First Commit

---
+++ src/test/java/firstPackage/TestClass.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    public void checkingNum() {
+        Assertions.assertTrue(2 < 5);
+    }
+}
